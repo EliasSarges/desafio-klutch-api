9518a8ce91eb49924d14085f09bdfc6c8495eb66
Elias Castro <eliassarges@outlook.com>
2021-06-01 20:11:55 -0300

    fix(controllers): fixed search return

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -16,7 +16,7 @@ module.exports = {
       `%${name}%`
     );
 
-    const data = { ...saleOffData, ...productsData };
+    const data = [...saleOffData, ...productsData];
 
     return res.status(200).json(data);
   },
