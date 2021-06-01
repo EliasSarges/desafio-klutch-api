e54f1c714986b124a0a3446d9837f18458e6c564
Elias Castro <eliassarges@outlook.com>
2021-06-01 19:19:24 -0300

    feat(controllers) : added search controller

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,23 @@
+const db = require("../database/connection");
+
+module.exports = {
+  async index(req, res) {
+    const { name } = req.query;
+
+    const saleOffData = await db("saleOff").where(
+      "description",
+      "like",
+      `%${name}%`
+    );
+
+    const productsData = await db("products").where(
+      "description",
+      "like",
+      `%${name}%`
+    );
+
+    const data = { ...saleOffData, ...productsData };
+
+    return res.status(200).json(data);
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ const routes = Router();
 
 const ProductsController = require("./controllers/ProductsController");
 const SaleOffController = require("./controllers/SaleOffController");
+const SearchController = require("./controllers/SearchController");
 
 routes.get("/products", ProductsController.index);
 routes.get("/sale-off", SaleOffController.index);
+routes.get("/search", SearchController.index);
 
 routes.get("*", (req, res) => {
-  res.send("rota nao encontrada");
+  return res.status(404).json({ error: "rota nao encontrada" });
 });
 
 module.exports = routes;
